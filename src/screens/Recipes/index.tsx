e51189c9cb6276952557468b06fc09801854f44a
RafaeldeLimaThomaz <79thomaz@gmail.com>
2022-12-15 19:11:01 -0300

    alerts & discard button

--- src/screens/Recipes/index.tsx
+++ src/screens/Recipes/index.tsx
@@ -1,10 +1,22 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet, Button, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  useWindowDimensions,
+  FlatList,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import { NavigationContext } from "@react-navigation/native";
 import useList from "../../hooks/useList";
 import listToArray from "../../helpers/listToArray";
 import RecipeShape from "../../types/RecipeShape";
 import { AppContext } from "../../contexts/AppContext";
+import { Card, Icon } from "react-native-elements";
+import { ScrollView } from "react-native-gesture-handler";
+import { getDatabase, ref, remove } from "firebase/database";
 
 const Recipes = () => {
   const appContext = useContext(AppContext);
@@ -12,18 +24,133 @@ const Recipes = () => {
   const recipes = useList("recipes");
   const recipesList = listToArray(recipes.data || {});
 
+  const width = useWindowDimensions().width * 0.6;
+
   const handleEdit = (refKey: string | undefined) => {
     if (refKey) navigation?.navigate("Recipe", { refKey });
   };
 
+  const handleDelete = (refKey: string | undefined) => {
+    return Alert.alert(
+      "Apagar Receita?",
+      "Excluir definitivamente esta receita?",
+      [
+        {
+          text: "Apagar",
+          onPress: () => {
+            remove(ref(getDatabase(), "recipes/" + refKey));
+          },
+          style: "destructive",
+        },
+
+        {
+          text: "Cancelar",
+        },
+      ]
+    );
+  };
+
   const renderItem = (item: RecipeShape) => (
-    <View>
-      <Text>{item.name}</Text>
-      <Text>{item.description}</Text>
-      <Text>{item.directions}</Text>
-      <Text>{JSON.stringify(item.ingredients)}</Text>
-      <Button title="Edit" onPress={() => handleEdit(item.key)} />
-    </View>
+    <ScrollView>
+      <Card
+        containerStyle={{
+          backgroundColor: "#EF3762",
+          borderRadius: 10,
+        }}
+      >
+        <Card.Title style={{ color: "white" }}>{item.name}</Card.Title>
+        <Card.Divider color="white" width={1} />
+        <View style={{ flexDirection: "row", justifyContent: "space-evenly" }}>
+          <View
+            style={{
+              width: width * 0.6,
+              height: width * 0.6,
+              marginBottom: width / 15,
+              backgroundColor: "#FF4984",
+              borderStyle: "dashed",
+              borderRadius: 10,
+              borderWidth: 2,
+              borderColor: "white",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Icon
+              name="image"
+              type="feather"
+              color={"white"}
+              tvParallaxProperties={undefined}
+            />
+          </View>
+          <View>
+            <Text style={{ color: "white" }}>
+              Descrição: {item.description}
+            </Text>
+            <Text style={{ color: "white" }}>
+              Modo de preparo:{item.directions}
+            </Text>
+          </View>
+        </View>
+
+        <Text style={{ color: "white", margin: 10 }}>
+          Ingredientes: {JSON.stringify(item.ingredients)}
+        </Text>
+        <Card.Divider color="white" width={1} />
+
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              backgroundColor: "white",
+              width: width * 0.6,
+              height: width / 6,
+              borderRadius: 50,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            onPress={() => {
+              navigation?.navigate("Receita Detalhada", {
+                body: "Receita Detalhada",
+              });
+            }}
+          >
+            <Text style={{ color: "black", fontWeight: "bold" }}>ABRIR</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <Icon
+              name="edit"
+              size={20}
+              type="feather"
+              color="green"
+              raised
+              onPress={() => handleEdit(item.key)}
+              tvParallaxProperties={undefined}
+            ></Icon>
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <Icon
+              name="trash"
+              size={20}
+              raised
+              onPress={() => {
+                handleDelete(item.key);
+              }}
+              type="feather"
+              color="#EF3762"
+              tvParallaxProperties={undefined}
+            ></Icon>
+          </TouchableOpacity>
+        </View>
+      </Card>
+    </ScrollView>
   );
 
   return (
@@ -33,7 +160,7 @@ const Recipes = () => {
       <FlatList
         data={recipesList}
         renderItem={(itemInfo) => renderItem(itemInfo.item)}
-      />
+      ></FlatList>
     </View>
   );
 };
